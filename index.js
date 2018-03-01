533877d5efb1c3c64df3afdefae9fa9de5d70f0e
sasopenna <salvopenna97@gmail.com>
2018-03-01 13:32:51 +0100

    from 8 to 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let agrid = blankGrid(4, 4);
 
 createHTMLgrid('table_grid', agrid);
 
-for (let x = 0; x < 8; x++) {
+for (let x = 0; x < 3; x++) {
   addNumber(agrid);
 }
 updateHTMLgrid("table_grid", agrid);
