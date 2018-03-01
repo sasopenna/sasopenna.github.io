63b56896d05ac30e48e01f953779137e8e899d69
sasopenna <salvopenna97@gmail.com>
2018-03-01 14:37:43 +0100

    completely re-constructured with a class

--- 2048/grid.js
+++ 2048/grid.js
@@ -1,231 +1,258 @@
-let blankGrid = (rows, cols) => {
-  let newgrid = [];
-  for(let i = 0; i < rows; i++) {
-    newgrid[i] = [];
-    for(let j = 0; j < cols; j++) {
-      newgrid[i][j] = 0;
+class Game2048 {
+  constructor(table_id, rows, cols) {
+    this.table_id = table_id;
+    this.rows = rows;
+    this.cols = cols;
+    this.grid = Game2048.blankGrid(rows, cols);
+    this.keys = [37, 38, 39, 40];
+    this.started = false;
+
+    this.createHTMLgrid();
+    for (let x = 0; x < 3; x++) {
+      this.addNumber();
     }
+    this.updateHTMLgrid();
   }
-  return newgrid;
-}
 
-let createHTMLgrid = (table_id, _grid) => {
-  let tab = document.getElementById(table_id);
+  createHTMLgrid() {
+    let tab = document.getElementById(this.table_id);
 
-  if(!tab) {
-    console.log("ERROR: Table not found");
-    return;
-  }
-  if(_grid.length == 0) {
-    console.log("ERROR: Grid not initialized");
-    return;
-  }
-  let rows = _grid.length;
-  let cols = _grid[0].length;
-
-  for(let i = 0; i < rows; i++) {
-    let row = tab.insertRow(0);
-
-    for(let j = 0; j < cols; j++) {
-      let index = j + i * cols;
-      //let cell = row.insertCell(j);
-      //cell.id = index;
-      row.insertCell(j);
+    if(!tab) {
+      console.log("ERROR: Table not found");
+      return;
     }
-  }
-}
+    if(this.grid.length == 0) {
+      console.log("ERROR: Grid not initialized");
+      return;
+    }
+    for(let i = 0; i < this.rows; i++) {
+      let row = tab.insertRow(0);
 
-let updateHTMLgrid = (table_id, _grid) => {
-  let tab = document.getElementById(table_id);
-
-  if(!tab) {
-    console.log("ERROR: Table not found");
-    return;
-  }
-  if(_grid.length == 0) {
-    console.log("ERROR: Grid not initialized");
-    return;
-  }
-  let rows = tab.rows.length;
-  let cols = tab.rows[0].cells.length;
-
-  for(let i = 0; i < rows; i++) {
-    for(let j = 0; j < cols; j++) {
-      let index = j + i * cols;
-      let cell = tab.rows[i].cells[j];
-      if(!cell) {
-        console.log("ERROR: Table not initialized")
-        return;
+      for(let j = 0; j < this.cols; j++) {
+        let index = j + i * this.cols;
+        row.insertCell(j);
       }
-      cell.innerHTML = (_grid[i][j] == 0) ? "" : _grid[i][j];
+    }
+  }
 
-      let search = _grid[i][j].toString();
-      let size = cellOptions[search].size;
-      if(_grid[i][j] > 2048) {
-        size -= (search.length - 4) * 2;
-        search = "-1";
+  updateHTMLgrid() {
+    let tab = document.getElementById(this.table_id);
+
+    if(!tab) {
+      console.log("ERROR: Table not found");
+      return;
+    }
+    if(this.grid.length == 0) {
+      console.log("ERROR: Grid not initialized");
+      return;
+    }
+
+    for(let i = 0; i < this.rows; i++) {
+      for(let j = 0; j < this.cols; j++) {
+        let index = j + i * this.cols;
+        let cell = tab.rows[i].cells[j];
+        if(!cell) {
+          console.log("ERROR: Table not initialized")
+          return;
+        }
+        cell.innerHTML = (this.grid[i][j] == 0) ? "" : this.grid[i][j];
+
+        let search = this.grid[i][j].toString();
+        let size = cellOptions[search].size;
+        if(this.grid[i][j] > 2048) {
+          size -= (search.length - 4) * 2;
+          search = "-1";
+        }
+        cell.style.color = cellOptions[search].color;
+        cell.style.background = cellOptions[search].bgcolor;
+        cell.style.fontSize = size;
       }
-      cell.style.color = cellOptions[search].color;
-      cell.style.background = cellOptions[search].bgcolor;
-      cell.style.fontSize = size;
-    }
-  }
-}
-
-let addNumber = (_grid) => {
-  if(_grid.length == 0) {
-    console.log("ERROR: Grid not initialized");
-    return;
-  }
-  let rows = _grid.length;
-  let cols = _grid[0].length;
-
-  let add = [];
-
-  for(let i = 0; i < rows; i++) {
-    for(let j = 0; j < cols; j++) {
-      if(_grid[i][j] !== 0) continue;
-      add.push({
-        r: i,
-        c: j
-      });
     }
   }
 
-  if(add.length > 0) {
-    let spot = add[Math.floor(Math.random() * add.length)];
-    _grid[spot.r][spot.c] = (Math.random() > 0.25) ? 2 : 4;
-  }
-}
+  addNumber() {
+    if(this.grid.length == 0) {
+      console.log("ERROR: Grid not initialized");
+      return;
+    }
+    let add = [];
 
-let operateArray = (arr) => {
-  let array = arr;
-  for(let i = 0; i < array.length; i++) {
-    if(array[i] == 0) continue;
-    for(let j = i+1; j < array.length; j++) {
-      if(array[j] == 0) continue;
-      if(array[i] != array[j]) break;
-      array[j] = 0;
-      array[i] *= 2;
-      break;
+    for(let i = 0; i < this.rows; i++) {
+      for(let j = 0; j < this.cols; j++) {
+        if(this.grid[i][j] !== 0) continue;
+        add.push({
+          r: i,
+          c: j
+        });
+      }
+    }
+
+    if(add.length > 0) {
+      let spot = add[Math.floor(Math.random() * add.length)];
+      this.grid[spot.r][spot.c] = (Math.random() > 0.25) ? 2 : 4;
     }
   }
-  return array;
-}
 
-let moveArray = (arr) => {
-  let array = arr;
-  for(let i = 0; i < array.length-1; i++) {
-    if(array[i] !== 0) continue;
-    for(let j = i+1; j < array.length; j++) {
-      if(array[j] == 0) continue;
-      array[i] = array[j];
-      array[j] = 0;
-      break;
+  combineNumbers(d) {
+    let dir = this.getIndexKey(d);
+    if(dir == -1) return;
+
+    if(this.grid.length == 0) {
+      console.log("ERROR: Grid not initialized");
+      return;
     }
-  }
-  return array;
-}
+    let array = [];
 
-let getIndex = (id, i, max = -1) => (id == 1) ? i : max - 1 - i;
+    if(dir % 2 == 0) {
+      for(let i = 0; i < this.rows; i++) {
+        if(dir == 0) array = this.grid[i];
+        else if(dir == 2) {
+          array = [];
+          for(let j = this.cols - 1; j >= 0; j--) {
+            array.push(this.grid[i][j]);
+          }
+        }
 
-let combineNumbers = (dir, _grid) => {
-  if(_grid.length == 0) {
-    console.log("ERROR: Grid not initialized");
-    return;
-  }
-  let rows = _grid.length;
-  let cols = _grid[0].length;
-  let array = [];
-
-  if(dir % 2 == 0) {
-    for(let i = 0; i < rows; i++) {
-      if(dir == 0) array = _grid[i];
-      else if(dir == 2) {
+        array = Game2048.operateArray(array);
+        for(let j = 0; j < this.cols; j++) {
+          this.grid[i][j] = array[Game2048.getIndex(dir+1, j, this.cols)];
+        }
+      }
+    } else {
+      for(let j = 0; j < this.cols; j++) {
         array = [];
-        for(let j = cols - 1; j >= 0; j--) {
-          array.push(_grid[i][j]);
+        if(dir == 1) {
+          for(let i = 0; i < this.rows; i++) {
+            array.push(this.grid[i][j]);
+          }
+        } else if(dir == 3) {
+          for(let i = this.rows - 1; i >= 0; i--) {
+            array.push(this.grid[i][j]);
+          }
         }
-      }
 
-      array = operateArray(array);
-      for(let j = 0; j < cols; j++) {
-        _grid[i][j] = array[getIndex(dir+1, j, cols)];
+        array = Game2048.operateArray(array);
+        for(let i = 0; i < this.rows; i++) {
+          this.grid[i][j] = array[Game2048.getIndex(dir, i, this.rows)];
+        }
       }
     }
-  } else {
-    for(let j = 0; j < cols; j++) {
-      array = [];
-      if(dir == 1) {
-        for(let i = 0; i < rows; i++) {
-          array.push(_grid[i][j]);
-        }
-      } else if(dir == 3) {
-        for(let i = rows - 1; i >= 0; i--) {
-          array.push(_grid[i][j]);
-        }
-      }
+    this.moveNumbers(dir);
+    this.updateHTMLgrid();
+  }
 
-      array = operateArray(array);
-      for(let i = 0; i < rows; i++) {
-        _grid[i][j] = array[getIndex(dir, i, rows)];
-      }
+  moveNumbers(dir) {
+    if(this.grid.length == 0) {
+      console.log("ERROR: Grid not initialized");
+      return;
     }
-  }
-  moveNumbers(dir, _grid);
-  updateHTMLgrid("table_grid", _grid);
-}
+    let array = [];
 
-let moveNumbers = (dir, _grid) => {
-  if(_grid.length == 0) {
-    console.log("ERROR: Grid not initialized");
-    return;
-  }
-  let rows = _grid.length;
-  let cols = _grid[0].length;
-  let array = [];
+    if(dir % 2 == 0) {
+      for(let i = 0; i < this.rows; i++) {
+        if(dir == 0) array = this.grid[i];
+        else if(dir == 2) {
+          array = [];
+          for(let j = this.cols - 1; j >= 0; j--) {
+            array.push(this.grid[i][j]);
+          }
+       }
 
-  if(dir % 2 == 0) {
-    for(let i = 0; i < rows; i++) {
-      if(dir == 0) array = _grid[i];
-      else if(dir == 2) {
+        array = Game2048.moveArray(array);
+        for(let j = 0; j < this.cols; j++) {
+          this.grid[i][j] = array[Game2048.getIndex(dir+1, j, this.cols)];
+        }
+      }
+    } else {
+      for(let j = 0; j < this.cols; j++) {
         array = [];
-        for(let j = cols - 1; j >= 0; j--) {
-          array.push(_grid[i][j]);
-        }
-     }
+        if(dir == 1) {
+          for(let i = 0; i < this.rows; i++) {
+            array.push(this.grid[i][j]);
+          }
 
-      array = moveArray(array);
-      for(let j = 0; j < cols; j++) {
-        _grid[i][j] = array[getIndex(dir+1, j, cols)];
+        } else if(dir == 3) {
+          for(let i = this.rows - 1; i >= 0; i--) {
+            array.push(this.grid[i][j]);
+          }
+        }
+
+        array = Game2048.moveArray(array);
+        for(let i = 0; i < this.rows; i++) {
+          this.grid[i][j] = array[Game2048.getIndex(dir, i, this.rows)];
+        }
       }
     }
-  } else {
-    for(let j = 0; j < cols; j++) {
-      array = [];
-      if(dir == 1) {
-        for(let i = 0; i < rows; i++) {
-          array.push(_grid[i][j]);
-        }
 
-      } else if(dir == 3) {
-        for(let i = rows - 1; i >= 0; i--) {
-          array.push(_grid[i][j]);
-        }
-      }
-
-      array = moveArray(array);
-      for(let i = 0; i < rows; i++) {
-        _grid[i][j] = array[getIndex(dir, i, rows)];
-      }
-    }
+    this.addNumber();
   }
 
-  addNumber(_grid);
-}
+  getIndexKey(d) {
+    for(let k = 0; k < this.keys.length; k++) {
+      if(this.keys[k] != d) continue;
+      return k;
+    }
+    return -1;
+  }
 
-document.addEventListener('keydown', (event) => {
-  const k = event.keyCode - 37;
-  if (k < 0 || k > 3) return;
-  combineNumbers(k, agrid);
-});
+  setKeys(up, down, left, right) {
+    this.keys[0] = left;
+    this.keys[1] = up;
+    this.keys[2] = right;
+    this.keys[3] = down;
+  }
+
+  static startKeyListener(game) {
+    if(game.started) return;
+    game.started = true;
+
+    document.addEventListener('keydown', function(event) {
+      const k = event.keyCode;
+      game.combineNumbers(k);
+    });
+  }
+
+  static blankGrid(rows, cols) {
+    let newgrid = [];
+    for(let i = 0; i < rows; i++) {
+      newgrid[i] = [];
+      for(let j = 0; j < cols; j++) {
+        newgrid[i][j] = 0;
+      }
+    }
+    return newgrid;
+  }
+
+  static operateArray(arr) {
+    let array = arr;
+    for(let i = 0; i < array.length; i++) {
+      if(array[i] == 0) continue;
+      for(let j = i+1; j < array.length; j++) {
+        if(array[j] == 0) continue;
+        if(array[i] != array[j]) break;
+        array[j] = 0;
+        array[i] *= 2;
+        break;
+      }
+    }
+    return array;
+  }
+
+  static moveArray(arr) {
+    let array = arr;
+    for(let i = 0; i < array.length-1; i++) {
+      if(array[i] !== 0) continue;
+      for(let j = i+1; j < array.length; j++) {
+        if(array[j] == 0) continue;
+        array[i] = array[j];
+        array[j] = 0;
+        break;
+      }
+    }
+    return array;
+  }
+
+  static getIndex(id, i, max = -1) {
+    return (id == 1) ? i : max - 1 - i;
+  }
+}
